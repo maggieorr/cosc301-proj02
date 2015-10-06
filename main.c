18c8b791c5024b699c01268cfe12c4db34e08b59
Maggie <morr@colgate.edu>
2015-10-06 15:23:02 -0400

    commands separated and ignore comments

--- main.c
+++ main.c
@@ -10,8 +10,102 @@
 #include <sys/stat.h>
 #include <poll.h>
 #include <signal.h>
+#include <errno.h>
+
+int commentindex(const char *buffer){
+	int i =0;
+	while (buffer[i] != '\0'){
+		if (buffer[i]== '#'){
+			return i;			
+		}
+		i++;
+	}
+	return -1;
+}
+
+void removewhitespace(char s[]) {
+	int j=0;
+    	for(int i =0; i<strlen(s); i++){
+		if(!isspace(s[i])){
+			s[j]=s[i];
+			j+=1;
+}
+}
+	s[j]='\0';
+}
+
+char **separate_commands(const char *buffer[]){
+	char *copy = strdup(buffer);
+	char *semicolon= ";";
+	char *token;
+	char *tokencpy;
+	int num_commands=0;
+	for (token = strtok(copy,semicolon); token != NULL ; token= strtok(NULL,semicolon)){
+		num_commands ++;	
+	}
+	free(copy);
+	copy = strdup(buffer);
+	char **j = malloc((num_commands+1)*sizeof(char *));
+	int k = 0;
+	for(int i=0;i<num_commands;i++){
+		if (i==0){
+			token = strtok(copy, semicolon);
+			
+		}
+		else{
+			token = strtok(NULL,semicolon);
+		}
+		tokencpy= strdup(token);
+		removewhitespace(tokencpy);
+		if (strcmp(tokencpy,"")!=0){
+			j[k]=strdup(token);
+			k++;
+		}
+		free(tokencpy);
+			
+	}
+	j[k]=NULL;
+	free(copy);
+	return j;
+
+}
+
+void runsequential(char *commands[]){
+	printf("got here\n");
+	if (execv(commands[0],commands)<0){
+		fprintf(stderr, "exec failed:%s\n", strerror(errno));
+	}
+	
+}
+
+void runparallel(char *commands[]){
+
+}
 
 int main(int argc, char **argv) {
+
+    char buffer[1024];
+    printf("prompt:: ");
+    fflush(stdout);
+    bool sequential = true;
+    while (fgets(buffer, 1024, stdin) != NULL) {
+        int bufflen = strlen(buffer);
+        buffer[bufflen-1] = '\0';
+
+	int index = commentindex(buffer);
+	buffer[index] = '\0';
+	char **commands = separate_commands(buffer);
+	if (sequential==true){
+		printf("here\n");
+		runsequential(commands);
+	}
+	else{
+		runparallel(commands);
+	}
+	printf("prompt:: ");
+	fflush(stdout);
+    }
+    printf("bye now\n");
 
     return 0;
 }
