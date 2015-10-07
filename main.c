d8ce79d80a4a5c06e460a772c7a181faf6a4ed37
Maggie <morr@colgate.edu>
2015-10-07 00:38:17 -0400

    checks mode and runs sequential

--- main.c
+++ main.c
@@ -11,6 +11,9 @@
 #include <poll.h>
 #include <signal.h>
 #include <errno.h>
+#include <stdbool.h>
+
+static bool sequential = true;
 
 int commentindex(const char *buffer){
 	int i =0;
@@ -20,7 +23,7 @@ int commentindex(const char *buffer){
 		}
 		i++;
 	}
-	return -1;
+	return strlen(buffer);
 }
 
 void removewhitespace(char s[]) {
@@ -34,13 +37,12 @@ void removewhitespace(char s[]) {
 	s[j]='\0';
 }
 
-char **separate_commands(const char *buffer[]){
+char **separate_commands(const char *buffer[], const char *separateby){
 	char *copy = strdup(buffer);
-	char *semicolon= ";";
 	char *token;
 	char *tokencpy;
 	int num_commands=0;
-	for (token = strtok(copy,semicolon); token != NULL ; token= strtok(NULL,semicolon)){
+	for (token = strtok(copy,separateby); token != NULL ; token= strtok(NULL,separateby)){
 		num_commands ++;	
 	}
 	free(copy);
@@ -49,11 +51,11 @@ char **separate_commands(const char *buffer[]){
 	int k = 0;
 	for(int i=0;i<num_commands;i++){
 		if (i==0){
-			token = strtok(copy, semicolon);
+			token = strtok(copy, separateby);
 			
 		}
 		else{
-			token = strtok(NULL,semicolon);
+			token = strtok(NULL,separateby);
 		}
 		tokencpy= strdup(token);
 		removewhitespace(tokencpy);
@@ -69,34 +71,69 @@ char **separate_commands(const char *buffer[]){
 	return j;
 
 }
-
-void runsequential(char *commands[]){
-	printf("got here\n");
-	if (execv(commands[0],commands)<0){
-		fprintf(stderr, "exec failed:%s\n", strerror(errno));
+void print_tokens(char *tokens[]) {
+    int i = 0;
+    while (tokens[i] != NULL) {
+        printf("Token %d: %s\n", i+1, tokens[i]);
+        i++;
+    }
+}
+int runsequential(char *commands[]){
+//is this already parallel??
+	int i =0;
+	while (commands[i] != NULL){
+		char **args= separate_commands(commands[i], " \n\t");
+		if (strcmp(args[0], "mode")==0){
+			if (strcmp(args[1], "p")==0){
+				printf("got here\n");
+				sequential = false;
+				return 0;
+			}
+			else if (strcmp(args[1], "s")==0){
+				printf("got here\n");
+			}
+			else {
+				fprintf(stderr, "execv failed: %s\n", strerror(errno));
+			}
+			//need a case to wrongly execv all other modes? e.g. mode x
+			//currently prints <execv failed: Success> so prob need to do something dif
+		}
+		else{
+			pid_t pid = fork();
+			if (pid==0){
+				if (execv(args[0], args) < 0) {
+					fprintf(stderr, "execv failed: %s\n", strerror(errno));
+				}
+			}
+			if (pid >0){
+				int status=0;
+			   	wait(&status);
+			}
+		}
+		i++;
 	}
+	return 0;
 	
 }
 
 void runparallel(char *commands[]){
+	
 
 }
 
-int main(int argc, char **argv) {
 
+int main(int argc, char **argv) {
+    sequential = true;
     char buffer[1024];
     printf("prompt:: ");
     fflush(stdout);
-    bool sequential = true;
     while (fgets(buffer, 1024, stdin) != NULL) {
         int bufflen = strlen(buffer);
         buffer[bufflen-1] = '\0';
-
 	int index = commentindex(buffer);
 	buffer[index] = '\0';
-	char **commands = separate_commands(buffer);
-	if (sequential==true){
-		printf("here\n");
+	char **commands = separate_commands(buffer, ";");
+	if (sequential){
 		runsequential(commands);
 	}
 	else{
@@ -105,7 +142,7 @@ int main(int argc, char **argv) {
 	printf("prompt:: ");
 	fflush(stdout);
     }
-    printf("bye now\n");
+    printf("\nbye now\n");
 
     return 0;
 }
