f113774f36f1b0594068bf2306f457488d3868cb
Maggie <morr@colgate.edu>
2015-10-09 11:59:46 -0400

    everything function except background processing. need to make smaller functions

--- main.c
+++ main.c
@@ -14,10 +14,20 @@
 #include <stdbool.h>
 
 
+//Maggie Orr and Katey Loughran - we worked on the whole thing together
+
 struct Node {
         char dir[1024];
         struct Node *next;
         } ;
+        
+        
+struct job{
+	pid_t pidID;
+	char command[1024];
+	char status[10];
+	struct job *next;
+	};
 
 void free_commands(char **commands) {
     int i = 0;
@@ -50,7 +60,7 @@ void removewhitespace(char s[]) {
 	s[j]='\0';
 }
 
-char **separate_commands(const char *buffer[], const char *separateby){
+char **separate_commands(const char *buffer, const char *separateby){
 	char *copy = strdup(buffer);
 	char *token;
 	char *tokencpy;
@@ -112,6 +122,7 @@ bool pauseOrResume(char *args[]){
 int runsequential(char *commands[], bool *sequential, bool *ex, struct Node *head){
 	int i =0;
 	while (commands[i] != NULL){
+		struct Node *cpy= head;
 		char **args= separate_commands(commands[i], " \n\t");
 		if (strcmp(args[0], "mode")==0){
 		        if (args[1] == NULL) {			  
@@ -132,14 +143,20 @@ int runsequential(char *commands[], bool *sequential, bool *ex, struct Node *hea
 		else{
 		        struct stat statresult;
                         int rv = stat(args[0], &statresult);
-                        char *newarg[1024];
+                        char newarg[1024];
                         strncpy(newarg,args[0],1024);
-                        while (rv < 0) {
-                                strcpy(newarg,head->dir);
+                        while (rv < 0 && cpy!=NULL) {
+                                strcpy(newarg,cpy->dir);
                                 strcat(newarg,args[0]);
                                 rv = stat(newarg, &statresult);
-                                head=head->next;
+                                cpy=cpy->next;
                         }
+                        free(cpy);
+                        if (rv<0){
+                        	fprintf(stderr, "execv failed: %s\n", strerror(errno));
+                        }
+                        
+                        else{
 			pid_t pid = fork();
 			if (pid==0){
 				if (execv(newarg, args) < 0) {
@@ -149,6 +166,7 @@ int runsequential(char *commands[], bool *sequential, bool *ex, struct Node *hea
 			if (pid >0){
 				int status=0;
 			   	wait(&status);
+			}
 			}
 		}
 
@@ -163,9 +181,10 @@ int runsequential(char *commands[], bool *sequential, bool *ex, struct Node *hea
 
 int runparallel(char *commands[], bool *sequential, bool *ex, struct Node *head){
         
-        int i =0;
+       int i =0;
         int num_children=0;
 	while (commands[i] != NULL){
+		struct Node *cpy = head;
 		char **args= separate_commands(commands[i], " \n\t");
 		if (strcmp(args[0], "mode")==0){
 		        if (args[1] == NULL){
@@ -184,22 +203,29 @@ int runparallel(char *commands[], bool *sequential, bool *ex, struct Node *head)
 			*ex = true;
 		}
 		else{
-		        struct stat statresult;
+		       struct stat statresult;
                         int rv = stat(args[0], &statresult);
-                        char *newarg[1024];
+                        char newarg[1024];
                         strncpy(newarg,args[0],1024);
-                        while (rv < 0) {
-                                strcpy(newarg,head->dir);
+                        while (rv < 0 && cpy!=NULL) {
+                                strcpy(newarg,cpy->dir);
                                 strcat(newarg,args[0]);
                                 rv = stat(newarg, &statresult);
-                                head=head->next;
+                                cpy=cpy->next;
                         }
+                        free(cpy);
+                        if (rv<0){
+                        	fprintf(stderr, "execv failed: %s\n", strerror(errno));
+                        }
+                        
+                        else{
 		        num_children++;
 			pid_t pid = fork();
 			if (pid==0){
 				if (execv(newarg, args) < 0) {
 					fprintf(stderr, "execv failed: %s\n", strerror(errno));
 				}
+			}
 			}
 
 		}
@@ -214,7 +240,6 @@ int runparallel(char *commands[], bool *sequential, bool *ex, struct Node *head)
 	
 
 	return 0;
-	
 
 }
 
@@ -244,14 +269,15 @@ void free_dir(struct Node *dirlist) {
 
 
 int main(int argc, char **argv) {
-    bool *sequential = true;
-    bool *ex = false;
+    bool sequential = true;
+    bool ex = false;
     char buffer[1024];
     printf("prompt:: ");
     fflush(stdout);
-    struct Node *head = NULL;
+    struct Node *head1 = NULL;
+    struct job *head= NULL;
     FILE *datafile = fopen("shell-config", "r");
-    readfile(datafile,&head);
+    readfile(datafile,&head1);
     while (fgets(buffer, 1024, stdin) != NULL) {
         int bufflen = strlen(buffer);
         buffer[bufflen-1] = '\0';
@@ -259,14 +285,53 @@ int main(int argc, char **argv) {
 	buffer[index] = '\0';
 	char **commands = separate_commands(buffer, ";");
 	if (sequential){
-		runsequential(commands, &sequential, &ex, head);
+		runsequential(commands, &sequential, &ex, head1);
 	}
 	else{
-		runparallel(commands, &sequential, &ex,head);
+		while(1){
+				  // declare an array of a single struct pollfd
+		    struct pollfd pfd[1];
+		    pfd->fd = 0; // stdin is file descriptor 0
+		    pfd->events = POLLIN;
+		    pfd->revents = 0;
+		 
+		    // wait for input on stdin, up to 1000 milliseconds
+		    int rv = poll(&pfd[0], 1, 1000);
+		 
+		    // the return value tells us whether there was a 
+		    // timeout (0), something happened (>0) or an
+		    // error (<0).
+
+		    if (rv == 0) {
+			   
+		    } else if (rv > 0) {
+		    	char buffer[1024];
+			fgets(buffer, 1024, stdin);
+			int bufflen = strlen(buffer);
+			buffer[bufflen-1] = '\0';
+			int index = commentindex(buffer);
+			buffer[index] = '\0';
+			char **commands = separate_commands(buffer, ";");
+			runparallel(commands, &sequential, &ex,head1);
+			
+		    } else {
+			printf("there was some kind of error: %s\n", strerror(errno));
+		    }
+	
+	   	int status;
+		pid_t tostop;
+		tostop = waitpid(-1, &status, WNOHANG);
+		if (tostop > 0){
+			//findjob();
+			printf("Process %d completed!\n",tostop); 
+			//removejob(jobs, tostop);
+			
+		}
+		}	
 	}
 	free_commands(commands);
 	if (ex){
-	        free_dir(head);
+	        free_dir(head1);
 	        printf("bye now\n");
 	        exit(1);
 	}
@@ -276,8 +341,8 @@ int main(int argc, char **argv) {
 	
     }
     
-    printf("bye now\n");
-    free_dir(head);
+    printf("bye now!!\n");
+    free_dir(head1);
     return 0;
 }
 
